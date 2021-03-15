6f05de84ac46d7d7e114688e9f9760dd1f6ad0f7
Aishwarya Srivastava <aishwaryaaaaaaaaaaa@gmail.com>
2021-03-15 17:28:09 +0530

    day 5

---
+++ Day-5/src/Linkedlists.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Comparator;
+import java.util.List;
+public class Linkedlists {
+ public static void main(String args[])
+ {
+	 List<String> names=new LinkedList<String>();
+	 for(int i=0;i<50;i++)
+	 {
+	 names.add("Ram"+i);
+	 }
+	 names.sort(new Comparator <String>() {
+		 
+	@Override
+		public int compare(String o1,String o2) {
+			int value2=Integer.valueOf(o2.substring(3));
+			int value1=Integer.valueOf(o1.substring(3));
+			return value2 - value1;
+		}
+
+	
+	 });
+	 System.out.println(names);
+ }
+
+}
